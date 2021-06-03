39dce916c0905f82a07765bd1c5b61e70f016b02
walid abou ali <walid.abou.ali@ibm.com>
2021-06-04 02:56:49 +0400

    render basic List

--- src/components/GistList.js
+++ src/components/GistList.js
@@ -1,4 +1,16 @@
 import React, { useEffect, useState } from 'react'
-const GistList = () => <></>
+
+const GistList = (props) => {
+return (
+<div>
+{
+    props.list.map(element => {
+        return <div key={element.url}>{JSON.stringify(element)}</div>;
+    })
+}
+</div>
+);
+
+}
 
 export default GistList
