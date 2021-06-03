e4b1c71162fbac1ff9c9d214b1d380fd200ad323
walid abou ali <walid.abou.ali@ibm.com>
2021-06-04 02:39:43 +0400

    add gist and suername contexts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
+import { useState, useMemo } from 'react';
 import styled from 'styled-components'
 import Header from "./components/Header";
+import { GistContext } from './contexts/gistContext';
+import { UsernameContext } from './contexts/usernameContext';
 import GlobalStyles from "./GlobalStyle";
 
 const App = () => {
+  const [username, setUserName] = useState();
+  const [gistList, setGistList] = useState();
+  const usernameProviderValue = useMemo(() => ({username, setUserName}), [username, setUserName]);
+  const gistListProvider = useMemo(() => ({gistList, setGistList}), [gistList, setUserName]);
+
   return (
+
     <Wrapper className="App" data-testid="app">
+      <UsernameContext.Provider value={usernameProviderValue} >
+      <GistContext.Provider value={gistListProvider}>
       <Header />
+
+      </GistContext.Provider>
+
+      </UsernameContext.Provider>
       <GlobalStyles />
     </Wrapper>
   );
