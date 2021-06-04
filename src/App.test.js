e2dc6a97d20b1c8c019dfb7d4576ca05840f2af4
walid abou ali <walid.abou.ali@ibm.com>
2021-06-04 09:09:39 +0400

    App component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,68 @@
-import { render, screen } from '@testing-library/react';
+import { act, findByTestId, render } from '@testing-library/react';
 import App from './App';
+import * as gistService from './services/gistService';
+jest.mock('./components/Header', () => {
+  return ()=> <div id="header"></div>
+});
+jest.mock('./components/GistList', () => {
+  return ()=> <div id="list"></div>
+});
+describe('App', () => {
+  it('should render correctly if fetching gists succeeds', async()=> {
+    jest.spyOn(gistService,'getPublicGists').mockImplementation(() => {
+      return {
+        status: 200,
+        data: [{}],
+      }
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId('app')
-  expect(linkElement).toBeInTheDocument();
+  })
+  let container
+  await act(()=> {
+     container = render(<App />).container;
+
+  })
+      expect(container).toMatchSnapshot();
+  
+  
+
+  });
+  it('should display error if fetching gists fails', async ()=> {
+    jest.spyOn(gistService,'getPublicGists').mockImplementation(() => {
+      return {
+        status: 400,
+        data: [],
+      }
+    });
+
+      let container
+      await act(()=> {
+         container = render(<App />).container;
+    
+      });
+      expect(container).toMatchSnapshot();
+
+   
+        
+
+  });
+  it('should display no results message if gits result is empty', async ()=> {
+    jest.spyOn(gistService,'getPublicGists').mockImplementation(() => {
+      return {
+        status: 200,
+        data: [],
+      }
+    });
+
+      let container
+      await act(()=> {
+         container = render(<App />).container;
+    
+      });
+      expect(container).toMatchSnapshot();
+
+   
+        
+
+  });
+
 });
